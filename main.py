250af1ae05e7634d159253555b3665f33f80226c
Veronika <vkotsubiak@gmail.com>
2022-11-12 17:10:20 +0200

    Правила

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import sqlite3
 connection = sqlite3.connect('itstepDB.sl3', 5)
 cur = connection.cursor()
 
-cur.execute('CREATE TABLE users (name TEXT, login TEXT, password TEXT)')
+# cur.execute('CREATE TABLE users (name TEXT, login TEXT, password TEXT)')
+# connection.commit()
+
+#cur.execute('INSERT INTO users (name, login, password) VALUES("Veronika", "login12", "256847")')
+#connection.commit()
+
+#cur.execute('UPDATE users SET name = "Nika" WHERE rowid = 1')
+#connection.commit()
+
+#cur.execute('DELETE FROM users WHERE rowid = 3')
+#connection.commit()
+
+cur.execute('SELECT rowid, name, login, password FROM users')
 connection.commit()
-
-
+res = cur.fetchall()
+print(res)
 
 connection.close()
